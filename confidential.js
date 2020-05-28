5fa9431eabce8abc374d9960ccba96455651df30
2000Vlad <35380936+2000Vlad@users.noreply.github.com>
2020-05-28 22:35:17 +0300

    Delete confidential.js

--- confidential.js
+++
@@ -1,3 +0,0 @@
-exports.pass = 'samsung2000'
-exports.connString = 'Data Source=DESKTOP-PAP9J4B\SQLEXPRESS;Initial Catalog=persondb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False'
-exports.server = 'localhost'//'DESKTOP-PAP9J4B\\SQLEXPRESS'
